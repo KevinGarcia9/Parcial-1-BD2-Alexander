0886cdb4399681d1c9911b387c0d2162765b54dd
Alexander García <kevider99@gmail.com>
2025-02-26 23:05:04 -0600

    se agregan las funcionalidades de Store Procedure

--- CapaDatos/CD_Conexion.cs
+++ CapaDatos/CD_Conexion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,22 @@ namespace CapaDatos
 {
     public class CD_Conexion
     {
-       // SqlConnection 
+        private SqlConnection db_conexion = new SqlConnection("Data Source=DESKTOP-TGA3FFT\\SQLEXPRESS;" +
+            "Initial Catalog=db_AgenciaCarros;" +
+            "Integrated Security=True;Encrypt=False");
+
+        public SqlConnection MtdAbrirConexion()
+        {
+            if (db_conexion.State == ConnectionState.Closed)
+                db_conexion.Open();
+            return db_conexion;
+        }
+
+        public SqlConnection MtdCerrarConexion()
+        {
+            if (db_conexion.State == ConnectionState.Closed)
+                db_conexion.Close();
+            return db_conexion;
+        }
     }
 }
